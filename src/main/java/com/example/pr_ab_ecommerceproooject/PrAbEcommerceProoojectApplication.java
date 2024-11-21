4906a1867eeff28564b806ea5d0fbee0d8415881
irfankhansajid <irfankhansajid@gmail.com>
2024-11-22 01:49:20 +0600

    Project Initial set up 01:48 am 22/11/24

---
+++ src/main/java/com/example/pr_ab_ecommerceproooject/PrAbEcommerceProoojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pr_ab_ecommerceproooject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrAbEcommerceProoojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrAbEcommerceProoojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pr_ab_ecommerceproooject/PrAbEcommerceProoojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pr_ab_ecommerceproooject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrAbEcommerceProoojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
